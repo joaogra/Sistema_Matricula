fc65745757bc643310b61bdccb0194eac39507bd
joaogra <joao.granatto@estudante.ufjf.br>
2025-06-30 22:12:23 -0300

    começa a class Aluno

---
+++ src/main/java/org/example/Aluno.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aluno {
+
+    private String nome;
+    private String matricula;
+    private List<String> disciplinasCursadas = new ArrayList<>();
+    private List<String> planejamento = new ArrayList<>();
+
+    // Trocar o Array disciplinasCursadas p um "TAD" que guarda o código e a nota que o aluno teve.
+
+    public Aluno(String nome, String matricula, List<String> historico, List<String> plan){
+        this.nome = nome;
+        this.matricula = matricula;
+        this.disciplinasCursadas.addAll(historico);
+        this.planejamento.addAll(plan);
+    }
+}
