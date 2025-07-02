8412b9e777b760940b1a8726d74eb476910be74b
adrianm07 <adrianmaciel007@gmail.com>
2025-07-02 16:42:28 -0300

    cria Disciplina Obrigatoria

---
+++ src/main/java/org/example/DisciplinaObrigatoria.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class DisciplinaObrigatoria extends Disciplina{
+
+    public DisciplinaObrigatoria(String nome, String codigo, int cargaHoraria) {
+        super(nome, codigo, cargaHoraria);
+    }
+}
